06ac03a141d7fc7ab01e2dc8182fb28deb9157ff
JeongJaeSoon <wjdwotns1006@gmail.com>
2021-03-01 20:21:32 +0900

    [bug-fix] 라이딩 알림 확인

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -145,8 +145,7 @@ class UserController extends Controller
     //2.updated_at 필드 값-> 확인 날짜 데이터 삽입
     public function notificationCheck(Notification $notification)
     {
-        $noti_id = $notification['id'];
-        dd($noti_id);
+//        $noti_id = $notification['id'];
         $user_id = Auth::guard('api')->user()->getAttribute('id');
 
         $this->notifications->checkNotification($user_id, $notification);
