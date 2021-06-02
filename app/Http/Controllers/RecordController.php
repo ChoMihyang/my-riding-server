0a67df77ae6ca4f2de05c1415e3d849356d959b7
ParkJoongKyu <wndrb553@naver.com>
2021-06-02 21:22:00 +0900

    mongo localhost 수정

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -214,7 +214,7 @@ class RecordController extends Controller
         if ($resultData->isEmpty()) {
             return $this->responseAppJson(
                 "일지 기록이 없습니다.",
-                "homeValue",[],
+                "homeValue", [],
                 200
             );
         }
@@ -461,7 +461,7 @@ class RecordController extends Controller
     public
     function mongoRecordSave(array $response_data, int $recordId)
     {
-        $response = \Illuminate\Support\Facades\Http::post("http://localhost:3000/api/record/$recordId", [
+        $response = \Illuminate\Support\Facades\Http::post("http://127.0.0.1:3000/api/record/$recordId", [
             "records" => $response_data
         ]);
 
@@ -472,7 +472,7 @@ class RecordController extends Controller
     public
     function mongoRecordShow(int $recordId)
     {
-        $response = \Illuminate\Support\Facades\Http::get("http://localhost:3000/api/record/$recordId");
+        $response = \Illuminate\Support\Facades\Http::get("http://127.0.0.1:3000/api/record/$recordId");
 
         return $response->json();
     }
